d552d4c667f3614b5f0d764b7f66c5a826979b23
Dan T <daniel.reeves.taylor@gmail.com>
2017-09-27 18:44:42 -0600

    added sqlconnection

--- src/data/SQLConnection.java
+++ src/data/SQLConnection.java
@@ -3,21 +3,26 @@ package data;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 
 public class SQLConnection {
+    //Driver, DB URL, credentials
+    private static final String SQL_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String SQL_DB_URL = "jdbc:mysql://52.206.157.109:3306/U03HA0";
+    private static final String SQL_USERNAME = "U03HA0";
+    private static final String SQL_PASSWORD = "53687976576";
 
-    //load the driver
-    Class.forName("com.mysql.jdbc.Driver")
-// create a connection
 
 
-   private static Connection conn = null;
+    private static Connection conn = null;
+    private static Statement stmnt = null;
 
     static {
         try {
-            conn = DriverManager.getConnection("jdbc:mysql://52.206.157.109:3306/U03HA0", "U03HA0", "53687976576");
-        } catch (SQLException e) {
+            Class.forName(SQL_DRIVER);
+            conn = DriverManager.getConnection(SQL_DB_URL, SQL_USERNAME, SQL_PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
     }
